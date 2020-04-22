d0c034f2612847514c189c0cc680ccb9c49b8aee
Marco <17228786@life.hkbu.edu.hk>
2020-04-22 12:22:09 +0800

    GG

---
+++ config/policies.js
@@ -0,0 +1,48 @@
+/**
+ * Policy Mappings
+ * (sails.config.policies)
+ *
+ * Policies are simple functions which run *before* your actions.
+ *
+ * For more information on configuring policies, check out:
+ * https://sailsjs.com/docs/concepts/policies
+ */
+
+module.exports.policies = {
+
+  /*************************
+  *                                                                          *
+  * Default policy for all controllers and actions, unless overridden.       *
+  * (`true` allows public access)                                            *
+  *                                                                          *
+  *************************/
+
+  // '*': true,
+  AdminController: {
+    '*': 'isAdmin'
+  },
+  UserController: {
+    login: true,
+    register: true,
+    forgot: true,
+    '*': 'isUser',
+  },
+  MembershipController: {
+    
+    '*': 'isUser',
+  },
+  AtheleteController: {
+    '*': 'isUser',
+  },
+  CoachController: {
+    coachform_detail: true,
+    coach_record: true,
+    indexAER:true,
+    indexACRO:true,
+    indexMAG:true,
+    indexRG:true,
+    indexWAG:true,
+    indexTRA:true,
+    '*': 'isUser',
+  },
+};
