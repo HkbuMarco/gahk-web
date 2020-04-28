00433277b6460d34402d86e05566109eb1ec9136
Marco <17228786@life.hkbu.edu.hk>
2020-04-29 02:04:30 +0800

    sdf

---
+++ config/custom.js
@@ -0,0 +1,23 @@
+/**
+ * Custom configuration
+ * (sails.config.custom)
+ *
+ * One-off settings specific to your application.
+ *
+ * For more information on custom configuration, visit:
+ * https://sailsjs.com/config/custom
+ */
+
+module.exports.custom = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Any other custom config this Sails app should use during development.    *
+  *                                                                          *
+  ***************************************************************************/
+   mailgunDomain: 'sandbox9e65993d89a84765ba84aaf0723d6c30.mailgun.org',
+  mailgunSecret: '3686d1b1a27043115c90211ae925ab88-f135b0f1-3587b1dd',
+  mailgunFrom:'17228786@life.hkbu.edu.hk',
+  // …
+
+};
